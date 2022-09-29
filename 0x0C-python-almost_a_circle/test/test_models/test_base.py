511040d38897dd5d31ac2fe121b13301934ad39f
MyGabriel <gabriellordmanu@gmail.com>
2022-09-29 09:44:52 +0000

    models in #Pythonisawesome

---
+++ 0x0C-python-almost_a_circle/test/test_models/test_base.py
@@ -0,0 +1,251 @@
+#!/usr/bin/python3
+"""
+This module contains unit tests for base.py file
+"""
+
+
+import unittest
+import pep8
+import json
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+from io import StringIO
+from unittest.mock import patch
+
+
+class test_base(unittest.TestCase):
+    """
+    This class will unittest the Base class
+    """
+
+    def test_module_docstring(self):
+        """
+        Test base module documentation exists
+        """
+        self.assertTrue(len(__import__('models').base.__doc__) >= 1)
+
+    def test_class_docstring(self):
+        """
+        Test Base class documentation exists
+        """
+        self.assertTrue(len(__import__('models').base.Base.__doc__) >= 1)
+
+    def test_method_docstring(self):
+        """
+        Tests Base methods for documentation
+        """
+        self.assertTrue(len(Base.__doc__) >= 1)
+        self.assertTrue(len(Base.__init__.__doc__) >= 1)
+        self.assertTrue(len(Base.to_json_string.__doc__) >= 1)
+        self.assertTrue(len(Base.save_to_file.__doc__) >= 1)
+        self.assertTrue(len(Base.from_json_string.__doc__) >= 1)
+        self.assertTrue(len(Base.create.__doc__) >= 1)
+        self.assertTrue(len(Base.load_from_file.__doc__) >= 1)
+
+    def test_pep8_compliance(self):
+        """
+        Tests to ensure models/base and /tests/test_models/test_base.py
+        are pep8 compliant
+        """
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(["models/base.py"])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(["tests/test_models/test_base.py"])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_init(self):
+        """
+        Tests self.id is created correctly
+        """
+        self.base1 = Base(1)
+        self.base2 = Base(2)
+        self.base3 = Base(2)
+        self.base4 = Base(3)
+        self.base5 = Base(-42)
+        self.assertEqual(self.base1.id, 1)
+        self.assertEqual(self.base2.id, 2)
+        self.assertEqual(self.base3.id, 2)
+        self.assertEqual(self.base4.id, 3)
+        self.assertEqual(self.base5.id, -42)
+        self.assertEqual(self.base2.id, self.base3.id)
+
+    def test_bad_init(self):
+        """
+        Tests for bad values passed to Base.id
+        """
+        with self.assertRaises(TypeError):
+            Base(1, 12)
+
+    def test_odd_init(self):
+        """
+        Tests for strings input
+        """
+        self.base_string = Base("string")
+        self.assertEqual(self.base_string.id, "string")
+
+    def test_nu_objects(self):
+        """
+        Tests forcint __nb_objects = 0, then creating empty to see index
+        """
+        Base._Base__nb_objects = 0
+        self.assertEqual(Base._Base__nb_objects, 0)
+        Base()
+        self.assertEqual(Base._Base__nb_objects, 1)
+
+    def test_none_to_json_string(self):
+        """
+        Tests that None passed to_json_string is functioning correctly
+        """
+        json_none = Base.to_json_string(None)
+        self.assertEqual(json_none, "[]")
+        self.assertEqual(type(json_none), str file:
+            test_String = file.read()
+        self.assertEqual(test_String, read_string)
+
+    def test_save_to_file_square(self):
+        """
+        Tests improper use of save_to_file method
+        """
+        s1 = Square(10, 7, 6, 0)
+        s2 = Square(1, 2, 3, 4)
+        Square.save_to_file([s1, s2])
+        read_string = '[{"id": 0, "size": 10, "x": 7, "y": 6}, \
+{"id": 4, "size": 1, "x": 2, "y": 3}]'
+)
+
+        json_empty = Base.to_json_string([])
+        self.assertEqual(json_empty, "[]")
+        self.assertEqual(type(json_empty), str)
+
+    def test_to_json_string(self):
+        """
+        Tests that two objects go to json file properly
+        """
+        Base.base_nb__objects = 0
+        o1 = {"id": 420, "width": 7, "height": 14, "x": 0, "y": 2}
+        o2 = {"id": 98, "width": 2, "height": 4, "x": 2, "y": 0}
+        json_str = Base.to_json_string([o1, o2])
+        self.assertEqual(type(json_str), str)
+        self.assertEqual(len(json.loads(json_str)), 2)
+
+    def test_save_to_file_rectangle(self):
+        """
+        Tests proper use of save_to_file method
+        """
+        r1 = Rectangle(10, 7, 6, 8, 0)
+        Rectangle.save_to_file([r1])
+        read_string = '[{"id": 0, "width": 10, "height": 7, "x": 6, "y": 8}]'
+
+        with open("Rectangle.json", "r") as file:
+            test_String = file.read()
+        self.assertEqual(test_String, read_string)
+
+    def test_save_to_file_square(self):
+        """
+        Tests improper use of save_to_file method
+        """
+        s1 = Square(10, 7, 6, 0)
+        s2 = Square(1, 2, 3, 4)
+        Square.save_to_file([s1, s2])
+        read_string = '[{"id": 0, "size": 10, "x": 7, "y": 6}, \
+{"id": 4, "size": 1, "x": 2, "y": 3}]'
+
+        with open("Square.json", "r") as file:
+            test_String = file.read()
+        self.assertEqual(test_String, read_string)
+
+    def test_from_json_string(self):
+        """
+        Tests that a two item list functions properly
+        """
+        list_input = [
+            {"id": 420, "width": 7, "height": 14, "x": 0, "y": 2},
+            {"id": 98, "width": 2, "height": 4, "x": 2, "y": 0}
+        ]
+        json_list_input = Base.to_json_string(list_input)
+        list_output = Base.from_json_string(json_list_input)
+        self.assertTrue(type(list_output) is list)
+        self.assertTrue(type(json_list_input) is str)
+        self.assertEqual(len(list_output), 2)
+
+    def test_None_from_json_string(self):
+        """
+        Tests from_json_string method on None
+        """
+        list_input = None
+        json_list_input = Base.to_json_string(list_input)
+        list_output = Base.from_json_string(json_list_input)
+        self.assertTrue(type(list_output) is list)
+        self.assertTrue(type(json_list_input) is str)
+        self.assertEqual(len(list_output), 0)
+
+    def test_empty_from_json_string(self):
+        """
+        Tests from_json_string method on empty string
+        """
+        list_input = []
+        json_list_input = Base.to_json_string(list_input)
+        list_output = Base.from_json_string(json_list_input)
+        self.assertTrue(type(list_output) is list)
+        self.assertTrue(type(json_list_input) is str)
+        self.assertEqual(len(list_output), 0)
+
+    def test_create_rectangle(self):
+        """
+        Tests create method for rectangle
+        """
+        r1 = Rectangle(3, 5, 1, 0, 0)
+        r1_dictionary = r1.to_dictionary()
+        r2 = Rectangle.create(**r1_dictionary)
+        self.assertEqual(str(r1), str(r2))
+        self.assertNotEqual(r1, r2)
+
+    def test_create_square(self):
+        """
+        Tests create method for square
+        """
+        s1 = Square(3, 5, 1, 0)
+        s1_dictionary = s1.to_dictionary()
+        s2 = Square.create(**s1_dictionary)
+        self.assertEqual(str(s1), str(s2))
+        self.assertNotEqual(s1, s2)
+
+    def test_load_from_file_rectangle(self):
+        """
+        Tests proper use of load_from_file method on rectangle
+        """
+        r1 = Rectangle(10, 7, 2, 8, 0)
+        r2 = Rectangle(2, 4, 0, 0, 0)
+        list_rectangles_input = [r1, r2]
+        Rectangle.save_to_file(list_rectangles_input)
+
+        list_rectangles_output = Rectangle.load_from_file()
+
+        self.assertEqual(str(list_rectangles_input[0]),
+                         str(list_rectangles_output[0]))
+        self.assertEqual(str(list_rectangles_input[1]),
+                         str(list_rectangles_output[1]))
+
+    def test_load_from_file_square(self):
+        """
+        Tests proper use of load_from_file method on square
+        """
+        s1 = Square(10, 7, 2, 0)
+        s2 = Square(2, 4, 0, 0)
+        list_squares_input = [s1, s2]
+        Square.save_to_file(list_squares_input)
+
+        list_squares_output = Square.load_from_file()
+
+        self.assertEqual(str(list_squares_input[0]),
+                         str(list_squares_output[0]))
+        self.assertEqual(str(list_squares_input[1]),
+                         str(list_squares_output[1]))
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ 0x0C-python-almost_a_circle/test/test_models/test_rectangle.py
@@ -0,0 +1,175 @@
+#!/usr/bin/python3
+"""
+This is the unittest module for the rectangle class
+"""
+
+
+import unittest
+import pep8
+import json
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+from io import StringIO
+from unittest.mock import patch
+
+
+class test_rectangle(unittest.TestCase):
+    """
+    This class will unittest the Rectangle class
+    """
+
+    def test_module_docstring(self):
+        """
+        Test rectangle module documentation exists
+        """
+        self.assertTrue(len(__import__('models').rectangle.__doc__) >= 1)
+
+    def test_class_docstring(self):
+        """
+        Test Rectangle class documentation exists
+        """
+        self.assertTrue(len(__import__('models').rectangle.Rectangle.
+                        __doc__) >= 1)
+
+    def test_method_docstring(self):
+        """
+        Tests Rectangle methods for documentation
+        """
+        self.assertTrue(len(Rectangle.__doc__) >= 1)
+        self.assertTrue(len(Rectangle.__init__.__doc__) >= 1)
+        self.assertTrue(len(Rectangle.area.__doc__) >= 1)
+        self.assertTrue(len(Rectangle.display.__doc__) >= 1)
+        self.assertTrue(len(Rectangle.__str__.__doc__) >= 1)
+        self.assertTrue(len(Rectangle.update.__doc__) >= 1)
+        self.assertTrue(len(Rectangle.to_dictionary.__doc__) >= 1)
+
+    def test_pep8_compliance(self):
+        """
+        Tests to ensure models/rectangle and
+        /tests/test_models/rectangle_base.py are pep8 compliant
+        """
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(["models/rectangle.py"])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(["tests/test_models/test_rectangle.py"])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_make_correct_rectangle(self):
+        """
+        Tests for correct rectangle creation
+        """
+        rectangle1 = Rectangle(1, 2, 3, 4, 0)
+        self.assertEqual(rectangle1.width, 1)
+        self.assertEqual(rectangle1.height, 2)
+        self.assertEqual(rectangle1.x, 3)
+        self.assertEqual(rectangle1.y, 4)
+        self.assertEqual(rectangle1.id, 0)
+        rectangle2 = Rectangle(10, 10, 0, 0, -1)
+        self.assertEqual(rectangle2.width, 10)
+        self.assertEqual(rectangle2.height, 10)
+        self.assertEqual(rectangle2.x, 0)
+        self.assertEqual(rectangle2.y, 0)
+        self.assertEqual(rectangle2.id, -1)
+        rectangle3 = Rectangle(1, 2)
+        self.assertEqual(rectangle3.width, 1)
+        self.assertEqual(rectangle3.height, 2)
+        rectangle4 = Rectangle(1, 2, 3)
+        self.assertEqual(rectangle4.width, 1)
+        self.assertEqual(rectangle4.height, 2)
+        self.assertEqual(rectangle4.x, 3)
+        rectangle5 = Rectangle(1, 2, 3, 4)
+        self.assertEqual(rectangle5.width, 1)
+        self.assertEqual(rectangle5.height, 2)
+        self.assertEqual(rectangle5.x, 3)
+        self.assertEqual(rectangle5.y, 4)
+
+    def test_make_incorrect_rectangle(self):
+        """
+        Tests with errors in making a rectangle
+        """
+        with self.assertRaises(TypeError):
+            Rectangle()
+        with self.assertRaises(TypeError):
+            Rectangle(1, 1, 1, 1, 1, 1)
+        with self.assertRaises(TypeError):
+            Rectangle("width", 1, 1, 1)
+        with self.assertRaises(TypeError):
+            Rectangle(1, "height", 1, 1)
+        with self.assertRaises(TypeError):
+            Rectangle(1, 1, "x", 1)
+        with self.assertRaises(TypeError):
+            Rectangle(1, 1, 1, "y")
+        with self.assertRaises(ValueError):
+            Rectangle(0, 1, 1, 1)
+        with self.assertRaises(ValueError):
+            Rectangle(-4, 1, 1, 1)
+        with self.assertRaises(ValueError):
+            Rectangle(1, 0, 1, 1)
+        with self.assertRaises(ValueError):
+            Rectangle(1, -4, 0, 0)
+        with self.assertRaises(ValueError):
+            Rectangle(1, 1, -2, 0)
+        with self.assertRaises(ValueError):
+            Rectangle(1, 1, 0, -7)
+
+    def test_area(self):
+        """
+        Tests proper function of area method
+        """
+        rectangle1 = Rectangle(2, 4, 0, 0, 1)
+        self.assertEqual(rectangle1.area(), 8)
+        rectangle2 = Rectangle(1, 1, 0, 0, 1)
+        self.assertEqual(rectangle2.area(), 1)
+
+    def test_display(self):
+        """
+        Tests proper function of display method
+        """
+        rectangle1 = Rectangle(3, 3, 0, 0, 1)
+        rectangle2 = Rectangle(2, 2, 2, 2, 2)
+        with patch('sys.stdout', new=StringIO()) as out:
+            rectangle1.display()
+            self.assertEqual('###\n###\n###\n', out.getvalue())
+        with patch('sys.stdout', new=StringIO()) as out:
+            rectangle2.display()
+            self.assertEqual('\n\n  ##\n  ##\n', out.getvalue())
+
+    def test___str__(self):
+        """
+        Tests proper function of __str__ method
+        """
+        rectangle1 = Rectangle(2, 4, 0, 0, 42)
+        self.assertEqual(rectangle1.__str__(), "[Rectangle] (42) 0/0 - 2/4")
+        rectangle2 = Rectangle(3, 6, 0, 0, 1)
+        self.assertEqual(rectangle2.__str__(), "[Rectangle] (1) 0/0 - 3/6")
+
+    def test_update(self):
+        """
+        Test for proper use of update method
+        """
+        r1 = Rectangle(2, 3, 4, 1, 1)
+        r2 = Rectangle(2, 3, 4, 5, 2)
+        self.assertNotEqual(str(r1), str(r2))
+        r2.update(id=1, y=1)
+        self.assertEqual(str(r1), str(r2))
+        r3 = Rectangle(3, 6, 9, 12, 0)
+        r4 = Rectangle(1, 2, 3, 4, 0)
+        r4.update(0, 3, 6, 9, 12)
+        self.assertEqual(str(r3), str(r4))
+
+    def test_to_dictionary(self):
+        """
+        Tests for proper use of to_dictionary method
+        """
+        r1 = Rectangle(10, 2, 1, 9, 0)
+        r1_dictionary = r1.to_dictionary()
+        self.assertTrue(type(r1_dictionary) is dict)
+        self.assertTrue(type(r1) is Rectangle)
+
+if __name__ == '__main__':
+    unittest.main()
